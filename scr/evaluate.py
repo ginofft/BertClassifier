62456a8b44dd0bcddf33736bbfe223d4cb739e8f
ginofft <ntnghia.andrei@gmail.com>
2023-03-09 16:48:05 +0700

    lol

--- scr/evaluate.py
+++ scr/evaluate.py
@@ -5,3 +5,4 @@ def calculate_multi_label_accuracy(preds: torch.Tensor, targets: torch.Tensor):
         raise TypeError('input tensors must be on the same device')
     assert preds.shape == targets.shape, "the two input tensors must have the same shape"
     return (torch.sum(preds*targets).item() / torch.numel(preds))
+
