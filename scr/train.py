caa204405b8490c00fa642a8af4b58d311e71a17
ginofft <ntnghia.andrei@gmail.com>
2023-03-10 15:29:50 +0700

    add metrics into train.py

--- scr/train.py
+++ scr/train.py
@@ -2,19 +2,28 @@ import torch
 from torch.utils.data import DataLoader
 from .dataset import SmartCollator, SentenceLabelDataset
 from .evaluate import MultiLabelEvaluator
-import evaluate
+
+EVALUATION_MAP = {
+    'accuracy' : MultiLabelEvaluator.get_accuracy,
+    'precision' : MultiLabelEvaluator.get_precision,
+    'recall' : MultiLabelEvaluator.get_recall,
+    'f1' : MultiLabelEvaluator.get_F1,
+}
+sigmoid = torch.nn.Sigmoid()
 
 def train(
         train_set:SentenceLabelDataset,
         model,
         criterion,
         optimizer,
+        evaluator:MultiLabelEvaluator,
+        metrics = ['Accuracy'],
         device=torch.device("cuda"),
         batch_size=8,  
         epoch=1):
     
     collator = SmartCollator(pad_token_id = train_set.tokenizer.pad_token_id, nClasses=train_set.nClasses)
-    #metrics = evaluate.load("accuracy")
+    evalFuns = [EVALUATION_MAP[metric.lower()] for metric in metrics]
     dataloader = DataLoader(train_set, 
                             batch_size = batch_size, 
                             num_workers = 2, 
@@ -34,11 +43,13 @@ def train(
         embeddings = model(input_ids, attention_masks)
         loss = criterion(embeddings, targets).to(device)
         loss.backward()
-        optimizer.step()
+        optimizer.step()  
 
         batch_loss = loss.item()
         epoch_loss += batch_loss
 
+        probs = sigmoid(embeddings)
+        evaluator.add_batch(probs = probs, targets = targets)
         # # Calculate Metrics
         # preds = torch.argmax(embeddings, dim=1)
         # metrics.add_batch(references = labels, predictions = preds)
@@ -53,6 +64,11 @@ def train(
                                                         batch_loss))
             del batch_loss
     
+    metricResults = []
+    for fun in evalFuns:
+        metricResults.append(fun(evaluator))
+    
+    evaluator.clean()
     avg_loss = epoch_loss / n_batches
     # print('--> Epoch {} completed, train avg. loss: {:.6f}'.format(epoch, avg_loss))
     # print('Epoch {} Metrics: {}'.format(epoch,metrics.compute()))
@@ -60,15 +76,17 @@ def train(
     
     if device == torch.device('cuda'):
         torch.cuda.empty_cache()
-    return avg_loss#, metrics.compute()
+    return avg_loss, dict(zip(metrics, metricResults))
 
 def inference(testSet : SentenceLabelDataset,
         model,
         criterion, 
+        evaluator : MultiLabelEvaluator,
+        metrics = ['Accuracy'],
         device = torch.device('cuda'),
         batch_size=8,):
     collator = SmartCollator(pad_token_id = testSet.tokenizer.pad_token_id, nClasses=testSet.nClasses)
-    #metrics = evaluate.load("accuracy")
+    evalFuns = [EVALUATION_MAP[metric] for metric in metrics]
     dataloader = DataLoader(testSet, 
                             batch_size = batch_size, 
                             num_workers = 2, 
@@ -91,14 +109,19 @@ def inference(testSet : SentenceLabelDataset,
             batch_loss = loss.item()
             epoch_loss += batch_loss
 
-            # Calculate Metrics
-            # preds = torch.argmax(embeddings, dim=1)
-            # metrics.add_batch(references = labels, predictions = preds)
+            probs = sigmoid(embeddings)
+            evaluator.add_batch(probs = probs, targets = targets)
 
             del input_ids, attention_masks, embeddings
             del loss
             del batch_loss
     avg_loss = epoch_loss / n_batches
+
+    metricResults = []
+    for fun in evalFuns:
+        metricResults.append(fun(evaluator))
+    
+    evaluator.clean()
     
     # print('---> Inference loss: {:.6f}'.format(avg_loss), flush = True)
     # print('Inference Metrics: {}'.format(metrics.compute()), flush = True)
@@ -106,4 +129,4 @@ def inference(testSet : SentenceLabelDataset,
     
     if device == torch.device('cuda'):
         torch.cuda.empty_cache()
-    return avg_loss#, metrics.compute()
+    return avg_loss, dict(zip(metrics, metricResults))
