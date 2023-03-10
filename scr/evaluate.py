2fd253030b171e7a8339f97a64a177153baf0ff8
ginofft <ntnghia.andrei@gmail.com>
2023-03-10 11:29:43 +0700

    add recall into MultiLabelEvaluator

--- scr/evaluate.py
+++ scr/evaluate.py
@@ -9,6 +9,7 @@ class MultiLabelEvaluator(metaclass = Singleton):
 
         self.positive = None
         self.truePositive = None
+        self.falsePositive = None
 
         self._check_size()
 
@@ -21,15 +22,29 @@ class MultiLabelEvaluator(metaclass = Singleton):
     def _get_positives(self):
         self._check_size()
         if (self.positive != None) and (self.truePositive != None):
-            return self.positive, self.truePositive
+            return self.positive, self.truePositive, self.falsePositive
         preds = (self.probs > self.percent).int()
         self.positive = torch.logical_or(preds, self.targets)
         self.truePositive = torch.logical_and(preds, self.targets)
-        return self.positive, self.truePositive
+        self.falsePositive = torch.where(self.truePositive==0, preds, 0)
+        return self.positive, self.truePositive, self.falsePositive
     
     def get_accuracy(self):
-        positive, truePositive = self._get_positives()
-        classAccuracy = torch.sum(truePositive, dim=1) / torch.sum(positive, dim=1)
+        positive, truePositive, _ = self._get_positives()
+        classAccuracy = torch.sum(truePositive, dim=0) / torch.sum(positive, dim=0)
         accuracy = (torch.sum(truePositive) / torch.sum(positive)).item()
         return classAccuracy, accuracy
     
+    def get_precision(self):
+        positive, truePositive, falsePositive = self._get_positives()
+        preds = (self.probs > self.percent).int()
+        classPrecsion = torch.sum(truePositive, dim =0) / torch.sum(preds, dim =0)
+        precision = torch.sum(truePositive) / torch.sum(preds).item()
+        return classPrecsion, precision
+    
+    def get_recall(self):
+        positive, truePositive, falsePositive = self._get_positives()
+        preds = (self.probs > self.percent).int()
+        classRecall = torch.sum(truePositive, dim =0) / torch.sum(self.targets, dim =0)
+        recall = torch.sum(truePositive) / torch.sum(self.targets).item()
+        return classRecall, recall
