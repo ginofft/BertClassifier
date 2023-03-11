bc958dc43c4fd8231c4f496a534f5f6d526277ad
ginofft <ntnghia.andrei@gmail.com>
2023-03-11 17:17:01 +0700

    fix --metrics argument

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ parser.add_argument('--nEpochs', type = int, default=50, help='No. epochs')
 parser.add_argument('--saveEvery', type = int, default = 10, 
                     help='no. epoch before a save is created')
 parser.add_argument('--metrics', nargs = '+', 
-                    default='Accuracy', choices=['Accuracy', 'Recall', 'Precision', 'F1'],
+                    default=['Accuracy'], choices=['Accuracy', 'Recall', 'Precision', 'F1'],
                     help='The evaluation metric for multi-label classification')
 
 #Data paremters
