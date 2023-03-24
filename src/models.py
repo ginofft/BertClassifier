10b5851bbe99a58cad8f12a90e1bdf720c2488ab
Andrei <80506834+ginofft@users.noreply.github.com>
2023-03-24 10:44:23 +0700

    Add distil bert (#1)
    Add:
    - model.py : distilbert
    - main.py : model mapping with --encoder

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from torch import nn
-from transformers import BertModel
+from transformers import BertModel, DistilBertModel
 
 class BertMLPClassifier(nn.Module):
     """A sentence classfier obtained from attaching a MLP layer on top of BERT
@@ -16,11 +16,11 @@ class BertMLPClassifier(nn.Module):
         relu layer
     """
 
-    def __init__(self, nClasses = 151, dropout = 0.3, checkpoint='bert-base-uncased'):
+    def __init__(self, nClasses = 151, dropout = 0.3):
         super(BertMLPClassifier, self).__init__()
         self.nClasses = nClasses
 
-        self.bert = BertModel.from_pretrained(checkpoint)
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
         self.dropout = nn.Dropout(dropout)
         self.linear = nn.Linear(self.bert.config.hidden_size,
                                 self.nClasses)
@@ -35,15 +35,28 @@ class BertMLPClassifier(nn.Module):
         #x = self.relu(x)
         return x
 
-class BertCRFClassifier(nn.Module):
-    def __init__(self):
-        pass
-    def forward(self):
-        pass
+class DistilBertMLPClassifier(nn.Module):
+    """A sentence classifier obtained from attaching a MLP on top of DistilBert
 
-class BertSVMClassifier(nn.Module):
-    def __init__(self):
-        super().__init__(BertSVMClassifier, self).__init__()
-        pass
-    def forward(self):
-        pass
+    Attributes
+    ----------
+    """
+    def __init__(self, nClasses = 151, dropout =0.3):
+        super(DistilBertMLPClassifier, self).__init__()
+
+        self.nClasses = nClasses
+        self.distilBert = DistilBertModel.from_pretrained('distilbert-base-uncased')
+        self.pre_classifier = nn.Linear(768, 768)
+        self.tanh = nn.Tanh()
+        self.dropout = nn.Dropout(dropout)
+        self.classifier = nn.Linear(768, self.nClasses)
+
+    def forward(self, input_ids, masks):
+        output_1 = self.distilBert(input_ids = input_ids, attention_mask = masks)
+        h = output_1[0]
+        x = h[:,0]
+        x = self.pre_classifier(x)
+        x = self.tanh(x)
+        x = self.dropout(x)
+        x = self.classifier(x)
+        return x
